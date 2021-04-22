ea65a11596555c3ec0a9bf0d0155291f87791c97
Gj <gongjie0422@163.com>
2021-04-22 17:50:02 +0800

    :pencil2: modify

--- background.js
+++ background.js
@@ -34,6 +34,8 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.windows.create({
     url: "page/index.html",
     type: "popup",
+    width: 1200,
+    height: 600,
   });
 });
 
