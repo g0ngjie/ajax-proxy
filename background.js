03ed0f3eca7e86d0f24243d4f6c48e6e2eeaaada
Gj <gongjie0422@163.com>
2021-04-22 17:05:02 +0800

    fix: modify

--- background.js
+++ background.js
@@ -1,29 +1,49 @@
-
 // 接收page传来的信息，转发给content.js
-chrome.runtime.onMessage.addListener(msg => {
-  if (msg.type === '__ajax_proxy' && msg.to === 'background') {
-    chrome.storage.local.get(['globalSwitchOn', 'proxy_routes'], (result) => {
-      if (result.hasOwnProperty('globalSwitchOn')) {
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.type === "__ajax_proxy" && msg.to === "background") {
+    chrome.storage.local.get(["globalSwitchOn", "proxy_routes"], (result) => {
+      if (result.hasOwnProperty("globalSwitchOn")) {
         if (result.globalSwitchOn) {
-          postMessage({ type: '__ajax_proxy', to: 'content', key: 'globalSwitchOn', value: result.globalSwitchOn });
+          postMessage({
+            type: "__ajax_proxy",
+            to: "content",
+            key: "globalSwitchOn",
+            value: result.globalSwitchOn,
+          });
           chrome.browserAction.setIcon({ path: "/images/16.png" });
         } else {
           chrome.browserAction.setIcon({ path: "/images/16g.png" });
         }
       }
       if (result.proxy_routes) {
-        postMessage({ type: '__ajax_proxy', to: 'content', key: 'proxy_routes', value: result.proxy_routes });
+        postMessage({
+          type: "__ajax_proxy",
+          to: "content",
+          key: "proxy_routes",
+          value: result.proxy_routes,
+        });
       }
     });
   }
   chrome.tabs.query({}, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { ...msg, to: 'content' });
-  })
+    chrome.tabs.sendMessage(tabs[0].id, { ...msg, to: "content" });
+  });
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.windows.create({
     url: "page/index.html",
-    type: "popup"
-  })
+    type: "popup",
+  });
+});
+
+// 设置默认icon
+chrome.storage.local.get("globalSwitchOn", (result) => {
+  if (result.hasOwnProperty("globalSwitchOn")) {
+    if (result.globalSwitchOn) {
+      chrome.browserAction.setIcon({ path: "/images/16.png" });
+    } else {
+      chrome.browserAction.setIcon({ path: "/images/16g.png" });
+    }
+  }
 });
