5bddf85ca335d32149008dc5e7e6a800e8ce7399
Gj <gongjie0422@163.com>
2023-07-11 09:54:47 +0800

    fix: #39

--- packages/shell-chrome/src/content.ts
+++ packages/shell-chrome/src/content.ts
@@ -18,7 +18,7 @@ import { onLoadForDataConversion } from "@proxy/compatibility";
 
 // 在页面上插入代码
 const script = document.createElement("script");
-script.setAttribute("type", "module");
+script.setAttribute("type", "text/javascript");
 script.setAttribute("src", chrome.runtime.getURL("document.js"));
 document.documentElement.appendChild(script);
 
